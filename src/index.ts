08dd74d509223b934e2f5d4dc3c55442252e3221
Rodrigo Avellar <roavellarm@gmail.com>
2021-03-01 21:38:45 -0300

    Start starter

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,10 @@
-import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
-import path from 'path'
-import { connectDataBase } from './config'
-import { routes } from './routes/index'
+import { connectDataBase, startServer } from './config'
+import routes from './routes'
 
 dotenv.config()
-
-const app = express()
-
 const { DATA_BASE, PORT } = process.env
-
-app.use(express.json())
-app.use(cors())
-app.use(routes)
-
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
-
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`⚡ Server is running on port ${PORT} 🎉`))
 
 connectDataBase(String(DATA_BASE))
 
-export default app
+startServer(PORT || '8000', routes)
