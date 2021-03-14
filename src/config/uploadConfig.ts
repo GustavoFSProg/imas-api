e6f50e111b3daf6b91d5ee8c215b966d24881dbf
Rodrigo Avellar <roavellarm@gmail.com>
2021-03-13 22:59:40 -0300

    Refactor Multer

--- src/config/uploadConfig.ts
+++ src/config/uploadConfig.ts
@@ -1,7 +1,7 @@
 import multer from 'multer'
 import path from 'path'
 
-module.exports = {
+const imageUploader = multer({
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(req, file, cb) {
@@ -10,4 +10,6 @@ module.exports = {
       cb(null, filename)
     },
   }),
-}
+}).single('image')
+
+export default imageUploader
